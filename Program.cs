ba2ded9c02cdbb04ded6e3877373ef4d6cc89c16
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-20 10:33:57 +0100

    Flygplansprogrammet

---
+++ Program.cs
@@ -0,0 +1,10 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
