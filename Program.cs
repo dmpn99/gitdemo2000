b47c518eaaefddaead9f98377b06df9aeec5be69
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-20 10:46:59 +0100

    Hej

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+
+        Console.WriteLine("Det här är ett en flygtidskalkylator.");
     }
 }
