2433ef475fc835bdfeef2cb2f3df2b6ce4ba0f9b
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-22 13:36:52 +0100

    Update Program.cs
    Hejsan

--- Program.cs
+++ Program.cs
@@ -5,17 +5,43 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-
-        Console.WriteLine("*");
-        Console.WriteLine("\nVälkommen till flygtidsberäknaren!");
-        Console.WriteLine("\nVilket flyg vill du se detaljerad information om? (svara med en siffra)");
-        Console.WriteLine("1. Stockholm - New York");
-        Console.WriteLine("2. New York - Stockholm");
-        Console.WriteLine("3. Avslyuta programmet");
-        Console.Write("Skriv ditt val här: ");
-        int userChoice = Console.Parse(Console.ReadLine());
-        /// koll
-    
-    
-    Den här raden är ny!
+            bool isRunning = true;
+            
+            while (isRunning)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("*************************************************************************");
+                Console.WriteLine("\nVälkommen till flygtidsberäknaren!");
+                Console.WriteLine("\nVilket flyg vill du se detaljerad information om? (svara med en siffra)");
+                Console.WriteLine("1. Stockholm - New York");
+                Console.WriteLine("2. New York - Stockholm");
+                Console.WriteLine("3. Avsluta programmet");
+                Console.Write("Skriv ditt val här: ");
+                int userChoice = int.Parse(Console.ReadLine());
+                
+                if (userChoice == 1)
+                {
+                    Console.WriteLine("Du har valt Stockholm - New York");
+                    isRunning = false;
+                }
+                else if (userChoice == 2)
+                {
+                    Console.WriteLine("Du har valt New York - Stockholm");
+                    isRunning = false;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Felaktigt val, försök igen!");
+                
+                    
+                }
+            }
         
+
+
+        
+    }
+}    
+    
