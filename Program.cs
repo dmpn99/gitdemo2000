7eabd39b83a382ad5b2d114a5b45e71b51924544
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-22 13:52:45 +0100

    Update Program.cs
    Förslag1

--- Program.cs
+++ Program.cs
@@ -5,13 +5,24 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+            // Variabler
+            int timeHours = 7;
+            int timeMinutes = 25;
+            int sthlmToNyHours = 14;
+            int sthlmToNyMinutes = 03;
+            int nyToSthlmHours = 10;
+            int nyToSthlmMinutes = 10;
+            int timeDifference = 6;
+            
             bool isRunning = true;
+            
             
             while (isRunning)
             {
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine("*************************************************************************");
+                Console.WriteLine();
                 Console.WriteLine("\nVälkommen till flygtidsberäknaren!");
                 Console.WriteLine("\nVilket flyg vill du se detaljerad information om? (svara med en siffra)");
                 Console.WriteLine("1. Stockholm - New York");
@@ -24,16 +35,20 @@ internal class Program
                 {
                     Console.WriteLine("Du har valt Stockholm - New York");
                     isRunning = false;
+                    Console.WriteLine("Avgångstid från Stockholm: 14:03");
+                    Console.WriteLine("Ankomsttid till New York: " + (sthlmToNyHours + timeHours - timeDifference) + ":" + (sthlmToNyMinutes + timeMinutes));
                 }
                 else if (userChoice == 2)
                 {
                     Console.WriteLine("Du har valt New York - Stockholm");
                     isRunning = false;
+                    Console.WriteLine("Avgångstid från New York: 10:10");
+                    Console.WriteLine("Ankomsttid till Stockholm: " + (nyToSthlmHours + timeHours + timeDifference) + ":" + (nyToSthlmMinutes + timeMinutes));
                 }
                 else
                 {
                     
-                    Console.WriteLine("Felaktigt val, försök igen!");
+                    Console.WriteLine("\nFelaktigt val, försök igen!");
                 
                     
                 }
